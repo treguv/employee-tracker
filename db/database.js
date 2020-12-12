6979f789fda4240988ab848f9251dc696cb3d331
treguv <vladislavtregubov00@gmail.com>
2020-12-12 01:33:54 -0800

    init connection to db

---
+++ db/database.js
@@ -0,0 +1,8 @@
+const mysql = require("mysql2");
+//Connect to db
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "2000Vt!2000",
+  database: "employees",
+});
