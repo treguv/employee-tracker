f8a63345c29c47039d450983f69c5a47b4d2a07f
treguv <vladislavtregubov00@gmail.com>
2020-12-12 20:31:13 -0800

    Added the basic menu for selecting initial option

--- server.js
+++ server.js
@@ -1,2 +1,51 @@
 const express = require("express");
 const db = require("./db/database");
+const inquirer = require("inquirer");
+
+// all possible questions
+function startup() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "currentAction",
+        message: "What would you like to do?",
+        choices: [
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Add A Department",
+          "Add A Role",
+          "Add An Employee",
+          "Update An Employee Role",
+        ],
+      },
+    ])
+    .then((response) => {
+      console.log(response);
+      //set up cases to do each of the things
+      switch (response.currentAction) {
+        case "View All Departments":
+          console.log("view all dpt");
+          break;
+        case "View All Roles":
+          console.log("view all roles");
+          break;
+        case "View All Departments":
+          console.log("view all employees");
+          break;
+        case "Add A Department":
+          console.log("add dpt");
+          break;
+        case "Add A Role":
+          console.log("Add role");
+          break;
+        case "Add An Employee":
+          console.log("add employee");
+          break;
+        case "Update An Employee Role":
+          console.log("update employee");
+      }
+    });
+}
+startup();
